2ecc29184489db0750bba319b625cf43dfdcaa2b
Selina Burtscher <selina.burtscher@gmx.at.wtv>
2026-04-28 12:28:30 +0200

    LightController

--- SmarthomeAPI/src/Components/LightController.tsx
+++ SmarthomeAPI/src/Components/LightController.tsx
@@ -11,22 +11,10 @@ function LightController() {
         }
     };
 
-    // const handlePost = async () => {
-    //     try {
-    //         const res = await api.post("/values/a001", {
-    //             value: "0"
-    //         });
-    //         console.log(res.data);
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // };
-
     return (
         <div>
             <h1>LightController funktioniert</h1>
             <button onClick={handleGet}>GET</button>
-            {/* <button onClick={handlePost}>POST</button> */}
         </div>
     );
 }
