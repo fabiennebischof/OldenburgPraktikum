244ddba62913008b77bfb697cb44edd883c2b94c
Fabienne Bischof <fabienne.bischof22gmail.com>
2026-04-29 10:18:45 +0200

    attempt at checkbox

--- SmarthomeAPI/src/Components/LightController.tsx
+++ SmarthomeAPI/src/Components/LightController.tsx
@@ -3,9 +3,11 @@ import './LightController.css';
 
 function LightController() {
 
+    //all off
     const handlePutEverythingOut = async () => {
         try {
             const res = await api.put("/values/a02k", {
+                
                 value: 0
             });
             console.log(res.data);
@@ -24,28 +26,54 @@ function LightController() {
     };
 
     //Flur
-    const handlePut = async () => {
+    const handleLight1 = async () => {
         try {
-            const res = await api.put("/values/a001", {
-                value: 0
-            });
-            console.log(res.data);
-        } catch (err) {
+
+            const ourCheckbox = document.querySelector("#switch");
+
+            if(!document.getElementById("switch") == null) 
+            {
+
+                if(true)
+                {
+                    const res = await api.put("/values/a001", 
+                    {
+                    value: 0
+                    });
+                    console.log(res.data);
+                
+                    return;
+                }
+                
+            }
+        } 
+        catch (err) 
+        {
             console.error(err);
         }
     };
 
     return (
         <div>
-            <h1>LightController funktioniert</h1>
+            <p>Light 1</p>
+            <label onClick={handleLight1}>
+                <input type="checkbox" className="switch" id="switch"/>
+                <span className="slider"></span>
+            </label>
+
+            <br />
+
+            <button className="button" onClick={handlePutEverythingOut}>
+                PUT EVERYTHING OUT
+            </button>
+            <button className="button" onClick={handleLight1}>
+                Light 1
+            </button>
             <button className="button" onClick={handleGet}>
                 GET
             </button>
-            <button className="button" onClick={handlePut}>
+            <button className="button" onClick={handleLight1}>
                 PUT
-            </button>
-            <button className="button" onClick={handlePutEverythingOut}>
-                PUT EVERYTHING OUT
             </button>
         </div>
     );
