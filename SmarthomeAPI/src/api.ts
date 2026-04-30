8e755919b4f887027bfc94f9a5c90ee8e3418c1a
Fabienne Bischof <fabienne.bischof22gmail.com>
2026-04-30 11:35:00 +0200

    config constants

--- SmarthomeAPI/src/api.ts
+++ SmarthomeAPI/src/api.ts
@@ -3,9 +3,10 @@ import axios from "axios";
 const USERNAME = "csd";
 const PASSWORD = "csd";
 const TOKEN = "zuuQtRg4a2n2SDoY2Ad3Gnbv0BSSNsH0";
+const BASE_URL = "/api/v1"
 
 export const api = axios.create({
-    baseURL: "/api/v1",
+    baseURL: BASE_URL,
     auth: {
         username: USERNAME,
         password: PASSWORD
--- SmarthomeAPI/vite.config.ts
+++ SmarthomeAPI/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_IP = "https://192.168.137.26";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
     "/api": {
-      target: "https://192.168.137.26",
+      target: API_IP,
       changeOrigin: true,
       secure: false
     }}
