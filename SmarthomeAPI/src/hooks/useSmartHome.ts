41cace0d3ab7e637cd21b38d706606ff5fcbc8d2
Fabienne Bischof <fabienne.bischof22gmail.com>
2026-05-05 15:12:26 +0200

    api folder

--- SmarthomeAPI/src/hooks/useSmartHome.ts
+++ SmarthomeAPI/src/hooks/useSmartHome.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { api } from "../api";
+import { api } from "../Api/api";
 import { LIGHTS } from "../constants/lights";
 import type { Room } from "../constants/lights";
 
